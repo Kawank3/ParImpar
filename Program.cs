0325842a77f3c58d65753c79d6dd5876229b0351
Kawan Weber <kawan_weber@hotmail.com>
2025-05-18 10:44:34 -0300

    ParImpar

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Digite um número: ");
+string entrada = Console.ReadLine()!;
+
+if (int.TryParse(entrada, out int numero))
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine(numero + " é par");
+    }
+    else
+    {
+        Console.WriteLine(numero + " é ímpar");
+    }
+}
+else
+{
+    Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+}
